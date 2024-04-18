5f0b1898b3c554f04507e1d953de7cf0198658bb
David Carlos <ddavidcarlos1392@gmail.com>
2024-04-18 12:56:26 -0300

    refactor: migrates bot/domain.default.yml
    Migrates the domain file to rasa 3

--- bot/custom_channels.py
+++ bot/custom_channels.py
@@ -33,130 +33,130 @@ class RocketChatInputChannel(RocketChatInput):
         return metadata
 
 
-class TelegramInputChannel(TelegramInput):
-    @classmethod
-    def name(cls) -> Text:
-        return "telegram"
-
-    @staticmethod
-    def _is_location(message: Message) -> bool:
-        return message.location is not None
-
-    @staticmethod
-    def _is_user_message(message: Message) -> bool:
-        return message.text is not None
-
-    @staticmethod
-    def _is_button(message: Update) -> bool:
-        return message.callback_query is not None
-
-    @staticmethod
-    def _is_edited_message(message: Update) -> bool:
-        return message.edited_message is not None
-
-    @staticmethod
-    def _is_command_valid(text) -> bool:
-        command_is_valid = False
-        for command in AVAILABLE_COMMANDS:
-            regex = f".*{command}.*"
-            if re.search(regex, text):
-                command_is_valid = True
-        return command_is_valid
-
-    def blueprint(
-        self, on_new_message: Callable[[UserMessage], Awaitable[Any]]
-    ) -> Blueprint:
-        telegram_webhook = Blueprint("telegram_webhook", __name__)
-        out_channel = self.get_output_channel()
-
-        @telegram_webhook.route("/", methods=["GET"])
-        async def health(_: Request) -> HTTPResponse:
-            return response.json({"status": "ok"})
-
-        @telegram_webhook.route("/set_webhook", methods=["GET", "POST"])
-        async def set_webhook(_: Request) -> HTTPResponse:
-            s = out_channel.setWebhook(self.webhook_url)
-            if s:
-                logger.info("Webhook Setup Successful")
-                return response.text("Webhook setup successful")
-            else:
-                logger.warning("Webhook Setup Failed")
-                return response.text("Invalid webhook")
-
-        @telegram_webhook.route("/webhook", methods=["GET", "POST"])
-        async def message(request: Request) -> Any:
-            if request.method == "POST":
-
-                request_dict = request.json
-                update = Update.de_json(request_dict)
-                if not out_channel.get_me().username == self.verify:
-                    logger.debug("Invalid access token, check it matches Telegram")
-                    return response.text("failed")
-
-                if self._is_button(update):
-                    msg = update.callback_query.message
-                    text = update.callback_query.data
-                elif self._is_edited_message(update):
-                    msg = update.edited_message
-                    text = update.edited_message.text
-                else:
-                    msg = update.message
-                    if msg is None:
-                        logger.debug("UPDATE MSG IS NONE")
-                        return response.text("success")
-                    if self._is_user_message(msg):
-                        text = msg.text.replace("/bot", "")
-                        if msg.text[0] == "/":
-                            text = msg.text[1:]
-                            if not self._is_command_valid(text):
-                                logger.debug("UNAVAILABLE COMMAND")
-                                return response.text("success")
-                        logger.debug(text)
-                    elif self._is_location(msg):
-                        text = '{{"lng":{0}, "lat":{1}}}'.format(
-                            msg.location.longitude, msg.location.latitude
-                        )
-                    else:
-                        return response.text("success")
-                sender_id = msg.chat.id
-                metadata = self.get_metadata(request)
-                try:
-                    if text == (INTENT_MESSAGE_PREFIX + USER_INTENT_RESTART):
-                        await on_new_message(
-                            UserMessage(
-                                text,
-                                out_channel,
-                                sender_id,
-                                input_channel=self.name(),
-                                metadata=metadata,
-                            )
-                        )
-                        await on_new_message(
-                            UserMessage(
-                                "/start",
-                                out_channel,
-                                sender_id,
-                                input_channel=self.name(),
-                                metadata=metadata,
-                            )
-                        )
-                    else:
-                        await on_new_message(
-                            UserMessage(
-                                text,
-                                out_channel,
-                                sender_id,
-                                input_channel=self.name(),
-                                metadata=metadata,
-                            )
-                        )
-                except Exception as e:
-                    logger.error(f"Exception when trying to handle message.{e}")
-                    logger.debug(e, exc_info=True)
-                    if self.debug_mode:
-                        raise
-                    pass
-
-                return response.text("success")
-
-        return telegram_webhook
+# class TelegramInputChannel(TelegramInput):
+#     @classmethod
+#     def name(cls) -> Text:
+#         return "telegram"
+#
+#     @staticmethod
+#     def _is_location(message: Message) -> bool:
+#         return message.location is not None
+#
+#     @staticmethod
+#     def _is_user_message(message: Message) -> bool:
+#         return message.text is not None
+#
+#     @staticmethod
+#     def _is_button(message: Update) -> bool:
+#         return message.callback_query is not None
+#
+#     @staticmethod
+#     def _is_edited_message(message: Update) -> bool:
+#         return message.edited_message is not None
+#
+#     @staticmethod
+#     def _is_command_valid(text) -> bool:
+#         command_is_valid = False
+#         for command in AVAILABLE_COMMANDS:
+#             regex = f".*{command}.*"
+#             if re.search(regex, text):
+#                 command_is_valid = True
+#         return command_is_valid
+#
+#     def blueprint(
+#         self, on_new_message: Callable[[UserMessage], Awaitable[Any]]
+#     ) -> Blueprint:
+#         telegram_webhook = Blueprint("telegram_webhook", __name__)
+#         out_channel = self.get_output_channel()
+#
+#         @telegram_webhook.route("/", methods=["GET"])
+#         async def health(_: Request) -> HTTPResponse:
+#             return response.json({"status": "ok"})
+#
+#         @telegram_webhook.route("/set_webhook", methods=["GET", "POST"])
+#         async def set_webhook(_: Request) -> HTTPResponse:
+#             s = out_channel.setWebhook(self.webhook_url)
+#             if s:
+#                 logger.info("Webhook Setup Successful")
+#                 return response.text("Webhook setup successful")
+#             else:
+#                 logger.warning("Webhook Setup Failed")
+#                 return response.text("Invalid webhook")
+#
+#         @telegram_webhook.route("/webhook", methods=["GET", "POST"])
+#         async def message(request: Request) -> Any:
+#             if request.method == "POST":
+#
+#                 request_dict = request.json
+#                 update = Update.de_json(request_dict)
+#                 if not out_channel.get_me().username == self.verify:
+#                     logger.debug("Invalid access token, check it matches Telegram")
+#                     return response.text("failed")
+#
+#                 if self._is_button(update):
+#                     msg = update.callback_query.message
+#                     text = update.callback_query.data
+#                 elif self._is_edited_message(update):
+#                     msg = update.edited_message
+#                     text = update.edited_message.text
+#                 else:
+#                     msg = update.message
+#                     if msg is None:
+#                         logger.debug("UPDATE MSG IS NONE")
+#                         return response.text("success")
+#                     if self._is_user_message(msg):
+#                         text = msg.text.replace("/bot", "")
+#                         if msg.text[0] == "/":
+#                             text = msg.text[1:]
+#                             if not self._is_command_valid(text):
+#                                 logger.debug("UNAVAILABLE COMMAND")
+#                                 return response.text("success")
+#                         logger.debug(text)
+#                     elif self._is_location(msg):
+#                         text = '{{"lng":{0}, "lat":{1}}}'.format(
+#                             msg.location.longitude, msg.location.latitude
+#                         )
+#                     else:
+#                         return response.text("success")
+#                 sender_id = msg.chat.id
+#                 metadata = self.get_metadata(request)
+#                 try:
+#                     if text == (INTENT_MESSAGE_PREFIX + USER_INTENT_RESTART):
+#                         await on_new_message(
+#                             UserMessage(
+#                                 text,
+#                                 out_channel,
+#                                 sender_id,
+#                                 input_channel=self.name(),
+#                                 metadata=metadata,
+#                             )
+#                         )
+#                         await on_new_message(
+#                             UserMessage(
+#                                 "/start",
+#                                 out_channel,
+#                                 sender_id,
+#                                 input_channel=self.name(),
+#                                 metadata=metadata,
+#                             )
+#                         )
+#                     else:
+#                         await on_new_message(
+#                             UserMessage(
+#                                 text,
+#                                 out_channel,
+#                                 sender_id,
+#                                 input_channel=self.name(),
+#                                 metadata=metadata,
+#                             )
+#                         )
+#                 except Exception as e:
+#                     logger.error(f"Exception when trying to handle message.{e}")
+#                     logger.debug(e, exc_info=True)
+#                     if self.debug_mode:
+#                         raise
+#                     pass
+#
+#                 return response.text("success")
+#
+#         return telegram_webhook
