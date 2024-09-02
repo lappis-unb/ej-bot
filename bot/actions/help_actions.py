90d270482e87063ae2b4e0dfd8fa2e5f736d7ce2
David Carlos <ddavidcarlos1392@gmail.com>
2024-09-02 19:07:11 -0300

    feat: simplify the help stories by adding a new action

---
+++ bot/actions/help_actions.py
@@ -0,0 +1,59 @@
+from typing import Any, Dict, Text
+from actions.base_actions import CheckersMixin
+from rasa_sdk import Action, FormValidationAction
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk import Action, Tracker
+from enum import Enum
+
+
+class ResetHelpFormSlots(Action):
+    """
+    Rest help_form slots to allows the user to request the help_form again.
+    """
+
+    def name(self):
+        return "action_reset_help_slots"
+
+    def run(self, dispatcher, tracker, domain):
+        return [SlotSet("help_topic", None)]
+
+
+class HelpChoices(Enum):
+    """
+    Enumerator with a list of valid help options.
+    """
+
+    HELP_VOTING = "utter_explain_help_voting"
+    HELP_PLAN = "utter_explain_help_plan"
+    HELP_KNOW_MORE = "utter_explain_help_know_more"
+    HELP_AUTHENTICATION = "utter_explain_help_authentication"
+    HELP_LGPD = "utter_explain_help_lgpd"
+
+
+class ValidateHelpForm(FormValidationAction, CheckersMixin):
+    """
+    This action is called when the vote_form is active.
+    It shows a comment for user to vote on, and also their statistics in the conversation.
+
+    https://rasa.com/docs/rasa/forms/#using-a-custom-action-to-ask-for-the-next-slot
+    """
+
+    def name(self) -> Text:
+        return "validate_help_form"
+
+    def validate_help_topic(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+
+        try:
+            CHOICE = HelpChoices[slot_value.upper()]
+            dispatcher.utter_message(template=CHOICE.value)
+            return {"help_topic": slot_value}
+        except Exception as e:
+            raise e
